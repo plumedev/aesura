9ae509ae864241f0b2b8f8e42730e7747f518ee8
Plume <plume-dev@outlook.fr>
2023-11-13 15:18:35 +0100

    feat(router): creater views and design router elem

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+
+import { RouterLink, RouterView } from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTableColumns, faBuildingColumns } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTableColumns, faBuildingColumns);
+
 </script>
 
 <template>
   <header>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" class="router-link link-offset-2">
+          <font-awesome-icon :icon="['fas', 'table-columns']" />
+          Dashboard
+        </RouterLink>
+        <RouterLink to="/accounts" class="router-link link-offset-2">
+          <font-awesome-icon :icon="['fas', 'building-columns']" />
+          Comptes
+        </RouterLink>
       </nav>
   </header>
 
