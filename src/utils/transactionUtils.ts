28de1fca547177eaabc33e67b9cf2091d9e955fe
Plume <plume-dev@outlook.fr>
2023-11-16 23:41:43 +0100

    feat(chartjs): add chartJS and create first chart

---
+++ src/utils/transactionUtils.ts
@@ -0,0 +1,12 @@
+/** Data */
+import transactions from "@/data/transactions.json";
+
+export const getRevenus = () => {
+  const revenus = transactions.filter((transaction) => transaction.type === true);
+  return revenus;
+}
+
+export const getDepenses = () => {
+  const depenses = transactions.filter((transaction) => transaction.type === false);
+  return depenses;
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,92 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+/**  Font Awesome */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faXmark, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-
+/** Data */
 import transactions from "@/data/transactions.json";
 
-import { useTransactionStore } from '@/stores/transactions'
-import { onMounted } from 'vue';
+/** Utils */
+import { getRevenus, getDepenses } from '@/utils/transactionUtils';
+/** Vue chart.js */
+import { BarChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
-const getRevenus = () => {
-  const revenus = transactions.filter((transaction) => transaction.type === true);
-  console.log(revenus);
-  return revenus;
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(ChartDataLabels);
+
+const getChartData = () => {
+  let chartDataDepense: number = 0;
+  let chartDataRevenus: number = 0;
+
+  getRevenus().forEach(element => {
+    chartDataRevenus = chartDataRevenus + element.montant;
+  });
+
+  getDepenses().forEach(element => {
+    chartDataDepense = chartDataDepense + element.montant;
+  });
+
+  const chartDatas: [number, number] = [chartDataRevenus, chartDataDepense];
+  return chartDatas
 }
 
-const getDepenses = () => {
-  const depenses = transactions.filter((transaction) => transaction.type === false);
-  console.log(depenses);
-  return depenses;
-}
+const chartDatas = ref(getChartData());
 
 onMounted(() => {
   getRevenus();
   getDepenses();
 });
+
+const chartData = ref({
+  labels: ['Revenus', 'Dépenses'],
+  datasets: [{
+    data: getChartData(), // Exemple: 5000€ pour les revenus, 3000€ pour les dépenses
+    backgroundColor: ['#2CB67D', '#E16162'],
+    borderColor: [
+      '#001E1D',
+    ],
+    borderWidth: 2
+  }],
+});
+
+const options = ref({
+  scales: {
+    x: {
+      display: false, // Masquer l'axe X complètement
+    },
+    y: {
+      display: false, // Masquer l'axe Y complètement
+    },
+  },
+  plugins: {
+    legend: {
+      display: false, // Masquer la légende
+    },
+    datalabels: {
+      align: 'end',
+      anchor: 'end',
+      color: '#ffffff',
+      font: {
+        weight: 'bold',
+        family: 'proxima-nova',
+        size: '18px'
+      }, // Couleur du texte
+      formatter: (value: number) => {
+        return Math.ceil(value) + ' €'; // Affiche la valeur
+      },
+  },
+  layout: {
+    padding: 0, // Pas de padding
+  },
+  elements: {
+    line: {
+      borderWidth: 10, // Pas de bordure de ligne
+    },
+  },
+}});
 
 
 library.add(faXmark, faArrowLeft, faArrowRight);
@@ -62,12 +125,15 @@ library.add(faXmark, faArrowLeft, faArrowRight);
         </div>
         <div class="col-4">
           <div class="card w-100 h-100" style="width: 18rem;">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column align-items-center">
               <div id="month-selector" class="d-flex align-items-center justify-content-evenly">
                 <font-awesome-icon :icon="['fas', 'arrow-left']" />
                 <h3 class="card-title text-light">Novembre</h3>
                 <font-awesome-icon :icon="['fas', 'arrow-right']" />
               </div>
+              <BarChart :chart-data="chartData" :options="options" />
+              <h3 class="text-light mt-4">Budget disponible</h3>
+              <h5 class="text-light-green">104€</h5>
             </div>
           </div>
         </div>
