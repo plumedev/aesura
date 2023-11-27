7f506748b62acd83d604d53f35dd4ac79507453a
Plume <plume-dev@outlook.fr>
2023-11-27 20:20:21 +0100

    feat(database): add vuefire and make CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 import './assets/main.scss'
 import './assets/responsive.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { VueFire } from 'vuefire';
+
+
 import App from './App.vue'
 import router from './router'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBAFSDHZtbikCUQG_ARaM5Y3FDz7eNcc5w",
-  authDomain: "aesura-bec67.firebaseapp.com",
-  projectId: "aesura-bec67",
-  storageBucket: "aesura-bec67.appspot.com",
-  messagingSenderId: "492866098858",
-  appId: "1:492866098858:web:193dc527345b8e65b3e2d3",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-E712L0CT47"
 };
 
@@ -34,5 +34,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebase,
+  modules: [],
+} as any);
 
 app.mount('#app')
